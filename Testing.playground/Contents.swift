36c9bc790a36cb247abc7d37ca09d7c1447cb737
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-25 12:28:33 -0600

    Created varibles that used different Swift techniques

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -2,6 +2,25 @@
 
 import UIKit
 
-var myName  :String
-var otherName = "Words"
-var thisName  :String = "REEEEEEE"
+
+// String Change
+var favColor :String = "Blue"
+favColor = "Red"
+print(favColor)
+
+// String interpolation
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+// Counting Characters
+let myColor = "myColor = I like the colors blue and red"
+print(myColor)
+print("myColor has \(myColor.characters.count) characters")
+
+//Comparing Strings
+let statement = "I like the color blue"
+let sameStatement = "I like the color blue"
+if statement == sameStatement {
+        print("These two strings are considered equal")
+}
+
