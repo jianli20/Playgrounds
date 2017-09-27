22933c638fc65bc784bffed7f195c38c4ef841b7
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-27 11:54:24 -0600

    Added a playground for method testing, created 2 simple methods

---
+++ Methods and Controls.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple() -> Void
+{
+        print("noWAH is TOXIC")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+        let answer = "My name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "NOWAH")
+let words = "Marshall Mathers"
+aBitLessSimple(name: words)
