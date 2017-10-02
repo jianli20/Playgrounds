3dfbbccf2ffa63de40125258d45e2906ffad760c
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-10-02 12:51:18 -0600

    Created Classes playground, created classes,assigned numbers for them, and used changeInternalState

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var notesToday = "Using SWift to make classes and methods"
+
+public class SimpleClass
+{
+    private var name :String
+    
+    public init()
+    {
+        name = "My name is ....."
+    }
+    
+    public func getName() -> String
+    {
+        return name
+    }
+}
+var sample = SimpleClass()
+print(sample.getName())
+
+public class OtherClass
+{
+    private var favoriteNumber :Int
+    private var favoriteWord :String
+ 
+    public init()
+    {
+        favoriteNumber = Int()
+        favoriteWord = String()
+    }
+    
+    public init(favoriteNumber :Int, favoriteWord :String)
+    {
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeInternalState() -> Void
+    {
+        self.favoriteNumber *= 20
+        self.favoriteWord += " and more and more"
+    }
+}
+//Swift Version
+var secondClass = OtherClass()
+//Java would be OtherClass sample = new OtherClass();
+//it is also not  = OtherClass>init()
+var thirdSample = OtherClass(favoriteNumber: 8 , favoriteWord: "clownfiesta")
+secondClass.changeInternalState()
+thirdSample.changeInternalState()
--- Methods and Controls.playground/Contents.swift
+++ Methods and Controls.playground/Contents.swift
@@ -16,3 +16,33 @@ public func aBitLessSimple(name: String) -> Void
 aBitLessSimple(name: "NOWAH")
 let words = "Marshall Mathers"
 aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName : String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Jian" )
+
+public func counting() -> Int
+{
+        return 5 + 6
+}
+print("I am not \(counting()) years old")
+
+if(counting() < 10)
+{
+        print("math works in swift too")
+}
+else
+{
+        print("wrong.")
+}
+var test = 0
+
+while (test < counting())
+{
+        print("hahahahahaha")
+        test += 1
+}
+
+print("yay!")
